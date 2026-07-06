15804f9adb039106ac8a4f5029a236ef9a1e57a9
vladyslav-crunch <vladyslav.crunch@gmail.com>
2026-07-06 22:52:19 +0200

    feat(favicon): add development and production favicon SVGs

--- packages/astrea-frontend/src/main.tsx
+++ packages/astrea-frontend/src/main.tsx
@@ -3,6 +3,22 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+const faviconHref = import.meta.env.DEV
+  ? '/favicon-dev.svg'
+  : '/favicon-prod.svg'
+
+const existingIcon = document.querySelector("link[rel='icon']") as HTMLLinkElement | null
+
+if (existingIcon) {
+  existingIcon.href = faviconHref
+} else {
+  const iconLink = document.createElement('link')
+  iconLink.rel = 'icon'
+  iconLink.type = 'image/svg+xml'
+  iconLink.href = faviconHref
+  document.head.appendChild(iconLink)
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
